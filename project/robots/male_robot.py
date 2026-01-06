4c6b737eff8957547b60118a6b6bf21cc4a22d1b
Bo Kovach <bo.kovachh@gmail.com>
2026-01-06 16:31:15 +0200

    Add male robot to project

---
+++ project/robots/male_robot.py
@@ -0,0 +1,11 @@
+from project.robots.base_robot import BaseRobot
+
+
+class MaleRobot(BaseRobot):
+    INITIAL_WEIGHT = 9
+    INCREASES_WEIGHT = 3
+    def __init__(self, name: str, kind: str, price: float):
+        super().__init__(name, kind, price, weight=self.INITIAL_WEIGHT)
+
+    def eating(self):
+        self.weight += self.INCREASES_WEIGHT
